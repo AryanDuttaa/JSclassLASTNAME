914ddbea1450fd078a7607fe9ccdb9d090f121ef
Aryan Dutta <0939806@my.scccd.edu>
2023-05-11 19:19:57 -0700

    more week 4

--- 010-typeof/scripts/main.js
+++ 010-typeof/scripts/main.js
@@ -1,12 +1,12 @@
-let A = letter // A is now a letter
-console.log("A is letter which is typeof ", (typeof A))
+let A = 42 // A is now a number
+console.log("A is 42 which is typeof ", (typeof A))
 
-A = "alphabet" // A is now an alphabet
-console.log("A is alphabet which is typeof ", (typeof A))
+A = "bar" // A is now a string
+console.log("A is bar which is typeof ", (typeof A))
 
-A = true // A is now a true
+A = true // A is now a boolean
 console.log("A is true which is typeof ", (typeof A))
 
-A = letter // A is a letter
-const result = A + "1" // JavaScript coerces A to an alphabet, so it can be concatenated with the other operand
-console.log("result of A + 1 is typeof - ", (typeof result)) // A1
+A = 42 // A is a number
+const result = A + "1" // JavaScript coerces A to a string, so it can be concatenated with the other operand
+console.log("result of 42 + 1 is typeof - ", (typeof result)) // 421
